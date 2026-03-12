a149d6f5c8271a8f120994bb5a9d5b69abcd1395
Tillayevxusniddin <tillayevx1@gmail.com>
2026-03-12 10:49:35 +0500

    fix: change frontend port to 3002 (3000 in use)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       name: 'greet-frontend',
       cwd: './frontend',
       script: 'node_modules/.bin/next',
-      args: 'start -p 3000',
+      args: 'start -p 3002',
       instances: 1,
       autorestart: true,
       watch: false,
